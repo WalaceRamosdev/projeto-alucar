fae5d2f13724e4da1813216f8d5d571a5282c613
Walace Ramos <contatowalace.dev@gmail.com>
2026-02-05 19:33:29 -0300

    style: rebranding de AluCar para FletMobi

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'alucar',
+    database: process.env.DB_NAME || 'FletMobi',
     password: process.env.DB_PASSWORD || 'postgres',
     port: process.env.DB_PORT || 5432,
 });
@@ -21,3 +21,4 @@ module.exports = {
     query: (text, params) => pool.query(text, params),
     pool
 };
+
